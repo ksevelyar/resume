8ea969593e5d2aa89937ce20fc7fddb199c4a311
Sergey Zubkov <ksevelyar@gmail.com>
2019-06-12 19:55:15 +0300

    add PDF version, setup tree shaking for FontAwesome

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 require('./resume.sass')
-import '@fortawesome/fontawesome-free/js/all'
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faBook, faUniversity } from '@fortawesome/free-solid-svg-icons'
+import { faClock, faEnvelope, faFileAlt } from '@fortawesome/free-regular-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faCheckSquare, faBook, faUniversity,
+  faClock, faEnvelope, faFileAlt,
+  faGithub
+)
+dom.watch()
